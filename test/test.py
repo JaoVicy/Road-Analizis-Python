d61641c0ff07de6dbd903210802cd52414d1254a
JaoVicy <filipimjoa@gmail.com>
2025-07-03 15:35:00 -0300

    Fix .csv generator

--- test/test.py
+++ test/test.py
@@ -7,14 +7,14 @@ with open("simulacao_lidar.csv", "w", newline="") as csvfile:
     writer.writerow(["Tempo(ms)", "Distancia(mm)"])
     tempo_inicial = time.time()
     
-    for i in range(100):  # 100 medições simuladas
+    for i in range(15):  # 100 medições simuladas
         tempo_ms = int((time.time() - tempo_inicial) * 1000)
-        distancia = random.randint(100, 220) # Simula medidas de "buraco"
+        distancia = random.randint(90, 170) # Simula medidas de "buraco"
         writer.writerow([tempo_ms, distancia])
         print(f"{tempo_ms};{distancia}")
         time.sleep(0.5)  # espera 500ms igual ao Arduino
 
-        for _ in range(5): # Simula 5 leituras de "chão liso"
+        for _ in range(2): # Simula 5 leituras de "chão liso"
             tempo_ms = int((time.time() - tempo_inicial) * 1000)
             distancia = random.randint(62, 65)
             writer.writerow([tempo_ms, distancia])
